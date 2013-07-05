121a3e20e8d5754a7d207ea10cab05070ba6f151
m_yoshida <m_yoshida@e-mind.co.jp>
2013-07-05 17:50:20 +0900

    add utils class

---
+++ src/main/java/chapter18/topic1/StringUtils.java
@@ -0,0 +1,9 @@
+package chapter18.topic1;
+
+public class StringUtils {
+
+	public static String toSnakeCase(String input) {
+		return "aaa";
+	}
+
+}
--- src/test/java/chapter18/topic1/StringUtilsTest.java
+++ src/test/java/chapter18/topic1/StringUtilsTest.java
@@ -2,13 +2,14 @@ package chapter18.topic1;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class StringUtilsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void _should_return_snake_case_input_aaa() {
+		assertThat(StringUtils.toSnakeCase("aaa"), CoreMatchers.is("aaa"));
 	}
 
 }
