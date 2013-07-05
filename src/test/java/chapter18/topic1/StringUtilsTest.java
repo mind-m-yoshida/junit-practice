2973823da72e533db8d67c78f5d7addb8e0c8569
m_yoshida <m_yoshida@e-mind.co.jp>
2013-07-05 17:46:44 +0900

    add test class

---
+++ src/test/java/chapter18/topic1/StringUtilsTest.java
@@ -0,0 +1,14 @@
+package chapter18.topic1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
