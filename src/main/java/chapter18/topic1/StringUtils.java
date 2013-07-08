bc3a35b01bbb6a9697c9c95623ac06dcdab7ff27
m_yoshida <m_yoshida@e-mind.co.jp>
2013-07-08 17:24:55 +0900

    finish

--- src/main/java/chapter18/topic1/StringUtils.java
+++ src/main/java/chapter18/topic1/StringUtils.java
@@ -6,19 +6,13 @@ import java.util.regex.Pattern;
 public class StringUtils {
 
 	public static String toSnakeCase(String input) {
-
-		Pattern pattern = Pattern.compile("[A-Z]");
-		
-		
-		
+		Pattern pattern = Pattern.compile("([A-Z])");
 		String target = input;
-		
 		Matcher matcher;
-		
 		while ((matcher = pattern.matcher(target)).find()) {
-			target = matcher.replaceFirst(matcher.group()) + "_";
+			target = matcher.replaceFirst("_" + matcher.group(1).toLowerCase());
 		}
-		return target;
+		return target.replaceFirst("^_", "");
 	}
 
 }
--- src/test/java/chapter18/topic1/StringUtilsTest.java
+++ src/test/java/chapter18/topic1/StringUtilsTest.java
@@ -1,20 +1,26 @@
 package chapter18.topic1;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class StringUtilsTest {
 
 	@Test
 	public void _should_return_snake_case_input_aaa() {
-		assertThat(StringUtils.toSnakeCase("aaa"), CoreMatchers.is("aaa"));
+		assertThat(StringUtils.toSnakeCase("aaa"), is("aaa"));
 	}
 
 	@Test
 	public void _should_return_snake_case_input_HelloWorld() {
-		assertThat(StringUtils.toSnakeCase("HelloWorld"), CoreMatchers.is("hello_world"));
+		assertThat(StringUtils.toSnakeCase("HelloWorld"), is("hello_world"));
+	}
+
+	@Test
+	public void _should_return_snake_case_input_practiceJunit() {
+		assertThat(StringUtils.toSnakeCase("practiceJunit"),
+				is("practice_junit"));
 	}
 
 }
